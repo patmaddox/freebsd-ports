e25271d232f3865701896412a73c546411f20834
Pat Maddox <pat@patmaddox.com>
2023-08-09 13:29:37 -0700

    cleanup

--- devel/py-tree_sitter/usr/local/share/licenses/py39-tree_sitter-0.20.1/catalog.mk
+++
@@ -1,5 +0,0 @@
-_LICENSE=MIT
-_LICENSE_NAME=MIT license / X11 license
-_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
-_LICENSE_GROUPS=COPYFREE FSF GPL OSI
-_LICENSE_DISTFILES=tree_sitter-0.20.1.tar.gz
